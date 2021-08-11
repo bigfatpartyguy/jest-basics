8857fa541fdf05c666b7678eebbff106fff89b34
Maksim Morozov <maxmorozov.art@gmail.com>
2021-08-11 23:02:53 +0300

    Exersice start

---
+++ unit_lesson/challenge/__tests__/get-token-from-header.test.js
@@ -0,0 +1,10 @@
+// Write unit tests for getTokenFromHeader.
+// Write at least two unit
+// tests to ensure that that use case is always supported.
+
+test('this is the title of your test', () => {
+  // this is where you put your test code. Write code that will
+  // throw an error if getTokenFromHeader has a bug. The `expect`
+  // global is a utility that makes writting such assertions easier,
+  // but you can do it however you like.
+});
