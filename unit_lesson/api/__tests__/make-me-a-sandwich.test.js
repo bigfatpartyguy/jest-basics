9cf8755d79b70d2428cb4bab8b0891ce0ad4fa22
Maksim Morozov <maxmorozov.art@gmail.com>
2021-08-11 22:58:39 +0300

    Create simplest tests

--- unit_lesson/api/__tests__/make-me-a-sandwich.test.js
+++ unit_lesson/api/__tests__/make-me-a-sandwich.test.js
@@ -1,1 +1,18 @@
-test('todo', () => {});
+import makeMeASandwich from '../make-me-a-sandwich';
+
+test('returns null when the sandwich does not exist', () => {
+  const req = {query: {}};
+
+  const res = makeMeASandwich(req);
+
+  expect(res).toBeNull();
+});
+
+test('returns sandwich when the sandwich exists', () => {
+  const req = {query: {sandwich: 'Denver Sandwich'}};
+  const expected = req.query.sandwich;
+
+  const res = makeMeASandwich(req);
+
+  expect(res).toBe(expected);
+});
