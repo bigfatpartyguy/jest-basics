9ef577785eb2ec8ebe1f0f8d555aec456d56d2a5
Maksim Morozov <maxmorozov.art@gmail.com>
2021-08-12 08:43:21 +0300

    Exercise finish

--- unit_lesson/challenge/__tests__/get-token-from-header.test.js
+++ unit_lesson/challenge/__tests__/get-token-from-header.test.js
@@ -1,10 +1,26 @@
 // Write unit tests for getTokenFromHeader.
 // Write at least two unit
 // tests to ensure that that use case is always supported.
+import getTokenFromHeader from '../get-token-from-header';
 
-test('this is the title of your test', () => {
-  // this is where you put your test code. Write code that will
-  // throw an error if getTokenFromHeader has a bug. The `expect`
-  // global is a utility that makes writting such assertions easier,
-  // but you can do it however you like.
+test('returns null when authorization does not exist', () => {
+  const req = {headers: {}, body: {}};
+
+  const res = getTokenFromHeader(req);
+
+  expect(res).toBeNull();
+});
+
+test('returns token when authorization exists', () => {
+  const req = {
+    headers: {
+      authorization: 'Token blahblahblahblah.blahblahblah.blahblahblah',
+    },
+    body: {},
+  };
+  const expected = req.headers.authorization.split(' ')[1];
+
+  const res = getTokenFromHeader(req);
+
+  expect(res).not.toBe(expected);
 });
