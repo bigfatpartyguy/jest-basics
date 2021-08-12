333147c56aa346f4cd538ff047aa769ad61c4b9a
Maksim Morozov <maxmorozov.art@gmail.com>
2021-08-12 09:58:09 +0300

    Exercise start

---
+++ tdd_lesson/challenge/__tests__/arrayify.test.js
@@ -0,0 +1,10 @@
+// You're going to develop a new utility function called `arrayify`
+// using Test Driven Development!
+// Requirements: accepts a single argument and returns an array
+// of that argument. If the given argument is already an array,
+// just return the argument. If it's given nothing, return an empty array.
+//
+// **Remember** to write a simple test to cover a simple use case. Then
+// write code to make that test pass. Then refactor your code to clean
+// it up if needed. Then continue the cycle until you cover all use cases.
+import arrayify from '../arrayify';
