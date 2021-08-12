70ba45c92276e15d2b427dce806240ebfa925b1d
Maksim Morozov <maxmorozov.art@gmail.com>
2021-08-12 10:55:19 +0300

    Exercise finish

--- tdd_lesson/challenge/__tests__/arrayify.test.js
+++ tdd_lesson/challenge/__tests__/arrayify.test.js
@@ -7,4 +7,34 @@
 // **Remember** to write a simple test to cover a simple use case. Then
 // write code to make that test pass. Then refactor your code to clean
 // it up if needed. Then continue the cycle until you cover all use cases.
+// arrayify() -> []
+// arrayify('hi') -> ['hi']
+// arrayify([1, 2, 3]) -> [1, 2, 3]
+
 import arrayify from '../arrayify';
+
+test('returns an empty array if nothing was given', () => {
+  const expected = [];
+
+  const res = arrayify();
+
+  expect(res).toEqual(expected);
+});
+
+test('accepts a single arg and retuns an array of that arg', () => {
+  const arg = 'hi';
+  const expected = ['hi'];
+
+  const res = arrayify(arg);
+
+  expect(res).toEqual(expected);
+});
+
+test('just returns an unchanged arg if it it an array', () => {
+  const arg = [1, 3, 2];
+  const expected = arg;
+
+  const res = arrayify(arg);
+
+  expect(res).toEqual(expected);
+});
--- tdd_lesson/challenge/arrayify.js
+++ tdd_lesson/challenge/arrayify.js
@@ -1,7 +1,7 @@
 export default arrayify;
-// Here's where you'll write arrayify. Look at
-// `api/src/routes/utils/arrayify.js` for instructions.
+// Here's where you'll write arrayify.
 
-function arrayify() {
+function arrayify(...args) {
   // TODO don't type in here until you have a unit test first!
+  return args.flat();
 }
